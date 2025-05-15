a16599384be0965249597dc11bb7e5daf0f74850
jw0921-kim <wlsdn921@nate.com>
2025-05-15 12:43:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
-const brotherGroups = {"겸손부": ["이태섭", "김진우", "이동환", "김지석", "전도영", "김권비", "김민호", "남준혁", "최재혁", "김인수", "문지훈", "전호성", "박지웅", "김두현", "박효원"], "온유부": ["양인수", "김호진", "이상수", "정형기", "박수용", "고병인", "이동길", "정인교", "권세빈", "김용빈", "김승후", "홍정의", "김민성", "하현성", "김진우2", "전은섭"], "사랑부": ["신창현", "임우혁", "정용진", "강민구", "노현준", "양홍열", "김해원", "백진호", "김교식", "이신섭", "조시형", "임향원", "심어진", "오서준", "임주용", "김도건", "이준희"]};
-const sisterGroups = {"겸손부": ["전효진", "고다영", "남소연", "김유나", "정여진", "심규리", "김민서", "박지혜", "심재경", "유지연", "심예림", "이소희", "임도해", "이현화", "김소은", "김이안"], "온유부": ["김유진", "이지희", "김민지", "임윤지", "박다은", "유재나", "김세연", "김채연", "임민해", "한예은", "임연주", "최연서", "한가은"], "사랑부": ["김혜련", "강민지", "최선영", "정희주", "박나은", "라유리", "하지원", "김호연", "유승비", "김주연", "강다빈", "이주화", "양정민", "한다빈", "유현지", "이다연"]};
+const brotherGroups = {
+  "겸손부": ["이태섭", "김진우", "이동환"],
+  "온유부": ["양인수", "김호진", "이상수"],
+  "사랑부": ["신창현", "임우혁", "정용진"]
+};
+const sisterGroups = {
+  "겸손부": ["전효진", "고다영", "남소연"],
+  "온유부": ["김유진", "이지희", "김민지"],
+  "사랑부": ["김혜련", "강민지", "최선영"]
+};
 const state = {};
 
 function render() {
@@ -33,10 +41,12 @@ function render() {
 }
 
 function toggle(name, div) {
-  if (!state[name]) state[name] = { car: true, flex: false, exclude: false };
-  else if (state[name].car) state[name] = { car: false, flex: true, exclude: false };
+  if (!state[name]) state[name] = { car: false, flex: false, exclude: false };
+  else if (state[name].exclude) state[name] = { car: false, flex: false, exclude: false };
   else if (state[name].flex) state[name] = { car: false, flex: false, exclude: true };
-  else state[name] = { car: false, flex: false, exclude: false };
+  else if (state[name].car) state[name] = { car: false, flex: true, exclude: false };
+  else state[name] = { car: true, flex: false, exclude: false };
+
   updateCard(div, name);
   localStorage.setItem("personStates", JSON.stringify(state));
 }
@@ -82,15 +92,6 @@ function assignCars() {
   document.getElementById("result").innerText = result;
   localStorage.setItem("lastAssignment", result);
   localStorage.setItem("personStates", JSON.stringify(state));
-
-  const neededBro = Math.max(0, Math.ceil(broPassengers.length / 3) - broDrivers.length);
-  const neededSis = Math.max(0, Math.ceil(sisPassengers.length / 3) - sisDrivers.length);
-  if (neededBro > 0 || neededSis > 0) {
-    let msg = "탑승 인원이 부족합니다.";
-    if (neededBro > 0) msg += `\n형제 차량이 최소 ${neededBro}대 더 필요합니다.`;
-    if (neededSis > 0) msg += `\n자매 차량이 최소 ${neededSis}대 더 필요합니다.`;
-    alert(msg);
-  }
 }
 
 function confirmReset(type) {
